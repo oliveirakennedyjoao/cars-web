b7bac1e485dd96b3f1f507a6905f9d92d046ea16
BR-FZ130195\João K. Oliveira <placeholder>
2021-07-01 21:39:19 -0300

    updating material 10

--- src/app/car/car-page/car-page.component.spec.ts
+++ src/app/car/car-page/car-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CarPageComponent } from './car-page.component';
 
@@ -6,7 +6,7 @@ describe('PageComponent', () => {
   let component: CarPageComponent;
   let fixture: ComponentFixture<CarPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CarPageComponent ]
     })
--- src/app/user/user-page/user-page.component.spec.ts
+++ src/app/user/user-page/user-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UserPageComponent } from './user-page.component';
 
@@ -6,7 +6,7 @@ describe('UserPageComponent', () => {
   let component: UserPageComponent;
   let fixture: ComponentFixture<UserPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UserPageComponent ]
     })
